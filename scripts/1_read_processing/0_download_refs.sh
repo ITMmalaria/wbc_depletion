ffea6d13deb89c995295cd81b662d006bb9296eb
Pieter Moris <13552343+pmoris@users.noreply.github.com>
2025-02-24 17:33:13 +0100

    Add stable URLs and simplify paths
    Use RefSeq 2023_10 release URL for human reference annotation.
    https://www.ncbi.nlm.nih.gov/refseq/annotation_euk/Homo_sapiens/GCF_000001405.40-RS_2023_10/
    
    Rename script and output directories for single refseq variant of script.

--- scripts/1_read_processing/0_download_refs.sh
+++ scripts/1_read_processing/0_download_refs.sh
@@ -6,8 +6,8 @@ set -euo pipefail
 # get file path of script
 # Otherwise, you would need to make sure to call the script from within the
 # directory where it is stored.
-if [ -n "${SLURM_JOB_ID:-}" ]; then
-    SCRIPT_DIR=$(dirname "$(scontrol show job "$SLURM_JOB_ID" | awk -F= '/Command=/{print $2}')")
+if [ -n "${SLURM_JOB_ID:-}" ] ; then
+    SCRIPT_DIR=$(dirname $(scontrol show job "$SLURM_JOB_ID" | awk -F= '/Command=/{print $2}'))
 else
     # SCRIPT_DIR=$(realpath "$0")
     SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
@@ -18,27 +18,31 @@ output_dir=$(realpath "${SCRIPT_DIR}/../../data/ref/")
 mkdir -p "${output_dir}"
 
 # download human reference genome and annotation
-wget -N -P "${output_dir}" "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_46/GRCh38.primary_assembly.genome.fa.gz"
-wget -N -P "${output_dir}" "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_46/gencode.v46.primary_assembly.basic.annotation.gff3.gz"
-wget -N -P "${output_dir}" "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_46/MD5SUMS"
+wget -N -P "${output_dir}" "https://ftp.ncbi.nlm.nih.gov/genomes/all/annotation_releases/9606/GCF_000001405.40-RS_2023_10/GCF_000001405.40_GRCh38.p14_genomic.fna.gz"
+wget -N -P "${output_dir}" "https://ftp.ncbi.nlm.nih.gov/genomes/all/annotation_releases/9606/GCF_000001405.40-RS_2023_10/GCF_000001405.40_GRCh38.p14_genomic.gff.gz"
+wget -N -P "${output_dir}" "https://ftp.ncbi.nlm.nih.gov/genomes/all/annotation_releases/9606/GCF_000001405.40-RS_2023_10/md5checksums.txt"
 
 # download pknowlesi reference genome and annotation
-wget -nc -P "${output_dir}" "https://plasmodb.org/common/downloads/release-68/PknowlesiH/fasta/data/PlasmoDB-68_PknowlesiH_Genome.fasta"
-wget -nc -P "${output_dir}" "https://plasmodb.org/common/downloads/release-68/PknowlesiH/gff/data/PlasmoDB-68_PknowlesiH.gff"
+wget -N -P "${output_dir}" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/006/355/GCF_000006355.2_GCA_000006355.2/GCF_000006355.2_GCA_000006355.2_genomic.fna.gz"
+wget -N -P "${output_dir}" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/006/355/GCF_000006355.2_GCA_000006355.2/GCF_000006355.2_GCA_000006355.2_genomic.gff.gz"
+wget -N -O "${output_dir}/md5checksums-pk.txt" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/006/355/GCF_000006355.2_GCA_000006355.2/md5checksums.txt"
 
 # Perform checksums
 cd "${output_dir}"
-sed -i '/MD5SUMS/d' "${output_dir}/MD5SUMS"
-if ! md5sum --ignore-missing -c "${output_dir}/MD5SUMS"; then
+if ! md5sum --ignore-missing -c "${output_dir}/md5checksums.txt" ; then
     echo "MD5 checksum failed for human reference genome..."
+    exit 1
+fi
+if ! md5sum --ignore-missing -c "${output_dir}/md5checksums-pk.txt" ; then
+    echo "MD5 checksum failed for Pk reference genome..."
     exit 1
 fi
 
 # concatenate human and pknowlesi genomes and annotation files
-gzip -c "${output_dir}/PlasmoDB-68_PknowlesiH_Genome.fasta" |
-    cat "${output_dir}/GRCh38.primary_assembly.genome.fa.gz" - \
-        >"${output_dir}/concat_GRCh38.14.gencode.46_PkH.PlasmoDB.68.fa.gz"
+cat "${output_dir}/GCF_000006355.2_GCA_000006355.2_genomic.fna.gz" \
+    "${output_dir}/GCF_000001405.40_GRCh38.p14_genomic.fna.gz" \
+    >"${output_dir}/concat-GCF_000001405.40_GRCh38.p14_genomic-GCF_000006355.2_GCA_000006355.2_genomic.fa.gz"
 
-gzip -c "${output_dir}/PlasmoDB-68_PknowlesiH.gff" |
-    cat "${output_dir}/gencode.v46.primary_assembly.basic.annotation.gff3.gz" - \
-        >"${output_dir}/concat_GRCh38.14.gencode.46_PkH.PlasmoDB.68.gff.gz"
+cat "${output_dir}/GCF_000006355.2_GCA_000006355.2_genomic.gff.gz" \
+    "${output_dir}/GCF_000001405.40_GRCh38.p14_genomic.gff.gz" \
+    >"${output_dir}/concat-GCF_000001405.40_GRCh38.p14_genomic-GCF_000006355.2_GCA_000006355.2_genomic.gff.gz"
