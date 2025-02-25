11fa131766b1b0f34ee732fcfb3f811683641c64
Pieter Moris <13552343+pmoris@users.noreply.github.com>
2025-02-25 15:34:27 +0100

    Clean up expression analysis scripts, add r objects and mapping stats
    Make refseq results the main ones, remove gencode attempts.
    Update script and rmarkdown report.
    Add mapping stats and manual curated mapping stats xlsx.
    Add r objects.
    Update renv/project files.
    Update readme.

--- scripts/2_expression_analysis/0_gene_annotations.sh
+++ scripts/2_expression_analysis/0_gene_annotations.sh
@@ -14,20 +14,12 @@ else
 fi
 
 # set input and output
-ref_human=$(realpath "${SCRIPT_DIR}/../../data/ref/gencode.v46.primary_assembly.basic.annotation.gff3.gz")
-ref_pk=$(realpath "${SCRIPT_DIR}/../../data/ref/PlasmoDB-68_PknowlesiH.gff")
+ref_human=$(realpath "${SCRIPT_DIR}/../../data/ref/GCF_000001405.40_GRCh38.p14_genomic.gff.gz")
+ref_pk=$(realpath "${SCRIPT_DIR}/../../data/ref/GCF_000006355.2_GCA_000006355.2_genomic.gff.gz")
 output_file=$(realpath "${SCRIPT_DIR}/../../data/ref/gene_types.csv")
 
 # create annotation file for rRNAs
+# unlike for gencode gff files, there is no need to ignore rRNA_pseudogene lncRNA
 echo "gene_type;gene_id" > "${output_file}"
-zcat "${ref_human}"| awk '$3 ~ /gene/' | cut -f9 | grep -E "gene_type=rRNA" | grep -v "rRNA_pseudo" | grep -o "ID=[^;]*" | sed 's/ID=/human_rRNA;/' >> "${output_file}"
-zcat "${ref_human}"| awk '$3 ~ /gene/' | cut -f9 | grep -E "gene_type=Mt_rRNA" | grep -o "ID=[^;]*" | sed 's/ID=/human_Mt_rRNA;/' >> "${output_file}"
-cut -f9 "${ref_pk}" | grep ";ebi_biotype=rRNA" | grep -o "ID=[^;]*" | sed 's/ID=/pk_rRNA;/' >> "${output_file}"
-
-# note: gene_ebi_biotype=rRNA entries are categorised as rRNA
-# grep "gene_ebi_biotype=rRNA" data/ref/PlasmoDB-68_PknowlesiH.gff | cut -f3 | sort -u
-# rRNA
-# while ebi_biotype=rRNA entries are ncRNA_genes
-# grep ";ebi_biotype=rRNA" data/ref/PlasmoDB-68_PknowlesiH.gff | cut -f3 | sort -u
-# ncRNA_gene
-# but the former are all transcripts with the latter as their parent
+zcat "${ref_human}" | awk '$3 ~ /gene/' | cut -f9 | grep -E "gene_biotype=rRNA" | grep -v "rRNA_pseudo" | grep -o "ID=[^;]*" | sed 's/ID=/human_rRNA;/' >> "${output_file}"
+zcat "${ref_pk}" | cut -f9 | grep -E "gene_biotype=rRNA" | grep -o "ID=[^;]*" | sed 's/ID=/pk_rRNA;/' >> "${output_file}"
