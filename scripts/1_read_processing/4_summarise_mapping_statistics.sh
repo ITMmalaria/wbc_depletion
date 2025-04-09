409acafcea112db37caf2fcb3f854fdf1c8b21bd
Pieter Moris <13552343+pmoris@users.noreply.github.com>
2025-04-09 16:28:52 +0200

    Improve mapping stats
    - Clarifies input and filtered fastq read counts (trimgalore vs star input)
    - Improves manual spreadsheet summary by adding
    gene-level counts and adds different flagstat numbers

--- scripts/1_read_processing/4_summarise_mapping_statistics.sh
+++ scripts/1_read_processing/4_summarise_mapping_statistics.sh
@@ -12,23 +12,22 @@ PROJECT_ROOT=$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null
 samplesheet="${PROJECT_ROOT}/data/samplesheet.csv"
 output="${PROJECT_ROOT}/results/mapping_stats.csv"
 
-echo "sample,condition,RNA_type,library_kit,removal,WBC_depletion,kit,trimgalore_total_1,trimgalore_filtered_1,trimgalore_total_2,trimgalore_filtered_2,star_input_reads,star_unique_mapped_n,star_unique_mapped_pct,star_splices_n,star_multiple_loci_n,star_multiple_loci_pct,star_too_many_loci_n,star_too_many_loci_pct,star_unmapped_mismatch_n,star_unmapped_mismatch_pct,star_unmapped_short_n,star_unmapped_short_pct,star_unmapped_other_n,star_unmapped_other_pct,star_chimeric_n,star_chimeric_pct,unique_mapped_pk,flagstat_total_all,flagstat_total_pk,flagstat_primary_all,flagstat_primary_pk,flagstat_secondary_all,flagstat_secondary_pk,flagstat_primary_mapped_all,flagstat_primary_mapped_pk,flagstat_proper_pair,flagstat_proper_pair_pk" > "${output}"
+echo "sample,condition,RNA_type,library_kit,removal,WBC_depletion,kit,fastq_read_pairs,filtered_read_pairs,star_unique_mapped_n,star_unique_mapped_pct,star_splices_n,star_multiple_loci_n,star_multiple_loci_pct,star_too_many_loci_n,star_too_many_loci_pct,star_unmapped_mismatch_n,star_unmapped_mismatch_pct,star_unmapped_short_n,star_unmapped_short_pct,star_unmapped_other_n,star_unmapped_other_pct,star_chimeric_n,star_chimeric_pct,unique_mapped_pk,flagstat_total_all,flagstat_total_pk,flagstat_primary_all,flagstat_primary_pk,flagstat_secondary_all,flagstat_secondary_pk,flagstat_primary_mapped_all,flagstat_primary_mapped_pk,flagstat_proper_pair,flagstat_proper_pair_pk" > "${output}"
 
 tail -n +2 "${samplesheet}" | \
     while IFS=, read -r sample fastq_1 fastq_2 strandedness condition RNA_type library_kit removal WBC_depletion kit; do
 
-        # extract stats from trimgalore
+        # count input fastq read pairs
         trim_log="${PROJECT_ROOT}/results/trimgalore/${sample}_1.fastq.gz_trimming_report.txt"
-        trimgalore_total_1=$(grep "Total reads processed" ${trim_log} | awk '{ print $NF }' | sed 's/,//g')
-        trimgalore_filtered_1=$(grep "Reads written (passing filters)" ${trim_log} | awk '{ print $(NF-1) }' | sed 's/,//g')
-        trim_log="${PROJECT_ROOT}/results/trimgalore/${sample}_2.fastq.gz_trimming_report.txt"
-        trimgalore_total_2=$(grep "Total reads processed" ${trim_log} | awk '{ print $NF }' | sed 's/,//g')
-        trimgalore_filtered_2=$(grep "Reads written (passing filters)" ${trim_log} | awk '{ print $(NF-1) }' | sed 's/,//g')
+        fastq_read_pairs=$(grep "Total reads processed" ${trim_log} | awk '{ print $NF }' | sed 's/,//g')
+
+        # note: filtered number is not correct because trimming due to length happens at the end
+        # after processing both r1 and r2 in trimgalore, instead grab the number from STAR's input
 
         # extract stats from STAR .Log.final.out
         log="${PROJECT_ROOT}/results/star_salmon/log/${sample}.Log.final.out"
 
-        star_input_reads=$(grep "Number of input reads" ${log} | cut -d '|' -f2 | tr -d '[:space:]')
+        filtered_read_pairs=$(grep "Number of input reads" ${log} | cut -d '|' -f2 | tr -d '[:space:]')
 
         star_unique_mapped_n=$(grep "Uniquely mapped reads number" ${log} | cut -d '|' -f2 | tr -d '[:space:]')
         star_unique_mapped_pct=$(grep "Uniquely mapped reads %" ${log} | cut -d '|' -f2 | tr -d '[:space:]')
@@ -57,7 +56,7 @@ tail -n +2 "${samplesheet}" | \
         # see: https://github.com/alexdobin/STAR/issues/507
         unique_mapped_pk=$(samtools view "${PROJECT_ROOT}/results/star_salmon/${sample}.markdup.sorted.pk.bam" | awk '/NH:i:1\t/ {print $1} ' | sort | uniq | wc -l)
 
-        # extract properly paired count from samtools flagstat
+        # extract numbers from samtools flagstat
         flagstat_total_all=$(sed '1q;d' "${PROJECT_ROOT}/results/star_salmon/samtools_stats/${sample}.markdup.sorted.bam.flagstat" | sed 's/ \+.*//g')
         flagstat_total_pk=$(sed '1q;d' "${PROJECT_ROOT}/results/star_salmon/samtools_stats_pk/${sample}.markdup.sorted.pk.bam.flagstat" | sed 's/ \+.*//g')
 
@@ -73,6 +72,6 @@ tail -n +2 "${samplesheet}" | \
         flagstat_proper_pair=$(sed '12q;d' "${PROJECT_ROOT}/results/star_salmon/samtools_stats/${sample}.markdup.sorted.bam.flagstat" | sed 's/ \+.*//g')
         flagstat_proper_pair_pk=$(sed '12q;d' "${PROJECT_ROOT}/results/star_salmon/samtools_stats_pk/${sample}.markdup.sorted.pk.bam.flagstat" | sed 's/ \+.*//g')
 
-    echo ${sample},${condition},${RNA_type},${library_kit},${removal},${WBC_depletion},${kit},${trimgalore_total_1},${trimgalore_filtered_1},${trimgalore_total_2},${trimgalore_filtered_2},${star_input_reads},${star_unique_mapped_n},${star_unique_mapped_pct},${star_splices_n},${star_multiple_loci_n},${star_multiple_loci_pct},${star_too_many_loci_n},${star_too_many_loci_pct},${star_unmapped_mismatch_n},${star_unmapped_mismatch_pct},${star_unmapped_short_n},${star_unmapped_short_pct},${star_unmapped_other_n},${star_unmapped_other_pct},${star_chimeric_n},${star_chimeric_pct},${unique_mapped_pk},${flagstat_total_all},${flagstat_total_pk},${flagstat_primary_all},${flagstat_primary_pk},${flagstat_secondary_all},${flagstat_secondary_pk},${flagstat_primary_mapped_all},${flagstat_primary_mapped_pk},${flagstat_proper_pair},${flagstat_proper_pair_pk} >> "${output}"
+    echo ${sample},${condition},${RNA_type},${library_kit},${removal},${WBC_depletion},${kit},${fastq_read_pairs},${filtered_read_pairs},${star_unique_mapped_n},${star_unique_mapped_pct},${star_splices_n},${star_multiple_loci_n},${star_multiple_loci_pct},${star_too_many_loci_n},${star_too_many_loci_pct},${star_unmapped_mismatch_n},${star_unmapped_mismatch_pct},${star_unmapped_short_n},${star_unmapped_short_pct},${star_unmapped_other_n},${star_unmapped_other_pct},${star_chimeric_n},${star_chimeric_pct},${unique_mapped_pk},${flagstat_total_all},${flagstat_total_pk},${flagstat_primary_all},${flagstat_primary_pk},${flagstat_secondary_all},${flagstat_secondary_pk},${flagstat_primary_mapped_all},${flagstat_primary_mapped_pk},${flagstat_proper_pair},${flagstat_proper_pair_pk} >> "${output}"
 
     done
